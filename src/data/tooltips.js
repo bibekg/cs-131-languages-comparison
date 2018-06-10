21b37cada785b9767e3548d0b882e05d45c2a850
Bibek <ghim.bibek@gmail.com>
2018-06-09 22:24:16 -0700

    Improve tooltip definitions

--- src/data/tooltips.js
+++ src/data/tooltips.js
@@ -1,19 +1,19 @@
 export const TOOLTIP_MAP = {
   scoping: {
-    static: 'A variable always refers to its top level environment',
-    dynamic: 'Occurrence of an identifier is searched in the most recent binding, compiler first searches current block and then successively all the calling functions'
+    static: 'In static scoping the compiler first searches in the current block, then in the surrounding blocks successively and finally in the global variables.',
+    dynamic: 'In dynamic scoping the compiler first searches the current block and then successively all the calling functions.'
   },
   typing: {
-    static: 'Types are checked at compile time (verifies and enforces the constraints of types on variables)',
-    dynamic: 'At runtime, the language system checks that operands are of suitable types for operators.',
-    weakly: 'There are holes in the type system that add flexibility but weaken the guarantee of operations on invalid operands. Does conversions between unrelated types implicitly.',
-    weak: 'There are holes in the type system that add flexibility but weaken the guarantee of operations on invalid operands. Does conversions between unrelated types implicitly.',
-    strongly: 'Languages checks the type of a variable before performing an operation on it. They require explicit casting and don’t allow implicit conversions.',
-    strong: 'Languages checks the type of a variable before performing an operation on it. They require explicit casting and don’t allow implicit conversions.'
+    static: 'Every variable name is bound to both an object and a type (at compile time via a data declaration).',
+    dynamic: 'Every variable name is bound only to an object (unless its null).',
+    weakly: 'Variables can be implicitly coerced to unrelated types. e.g. int x = 5.0 is legal',
+    weak: 'Variables can be implicitly coerced to unrelated types. e.g. int x = 5.0 is legal',
+    strongly: 'Type conversions must be explicit, e.g. in Python: print(str(4))',
+    strong: 'Type conversions must be explicit, e.g. in Python: print(str(4))',
   },
   'run-type': {
-    compiled: 'When a program, once compiled, is expressed in the instructions of the target machine',
-    interpreted: 'anguage where the instructions are not directly executed by the target machine, but instead read and executed by some other program'
+    compiled: 'Code is stored as a binary as instructions the CPU can directly execute.',
+    interpreted: 'Code is stored as source and must be translated to machine code by interpreter at runtime.'
   },
   'memory-allocation': {
     static: '',
